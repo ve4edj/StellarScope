2d2683866b379a813b0c88d81dcd60cc9c5b1941
Erik Johnson <erik@johnson.ac>
2015-04-12 00:27:18 -0500

    tweaked moving average

--- stellariumServer.py
+++ stellariumServer.py
@@ -1,4 +1,4 @@
-import stellariumConnect, angles, serial, datetime
+import stellariumConnect, angles, serial, datetime, numpy as np
 from reportCoordinates import reportCoordinates
 
 AVERAGE_LENGTH = 20.
@@ -8,22 +8,27 @@ stelCon.handshakeStellarium()
 stelCon.sendStellariumCoords(angles.Angle(r=0), angles.Angle(r=0))
 imu = serial.Serial('/dev/tty.usbmodem1411', 115200)
 try:
+	avgIdx = 0
 	while True:
-		yawAvg, pitchAvg, rollAvg, zoomAvg = (0,0,0,0)
+		yawAvg, pitchAvg, rollAvg, zoomAvg = (np.zeros(AVERAGE_LENGTH), np.zeros(AVERAGE_LENGTH), np.zeros(AVERAGE_LENGTH), np.zeros(AVERAGE_LENGTH))
 		for i in range(int(AVERAGE_LENGTH)):
 			reading = imu.readline().strip()
 			imuRes = reading.split(",")
 			if len(imuRes) <> 4:
 				continue
-			yawAvg += float(imuRes[0])
-			pitchAvg += float(imuRes[1])
-			rollAvg += float(imuRes[2])
-			zoomAvg += int(imuRes[3])
+			try:
+				yawAvg[avgIdx%AVERAGE_LENGTH] = float(imuRes[0])
+				pitchAvg[avgIdx%AVERAGE_LENGTH] = float(imuRes[1])
+				rollAvg[avgIdx%AVERAGE_LENGTH] = float(imuRes[2])
+				zoomAvg[avgIdx%AVERAGE_LENGTH] = int(imuRes[3])
+			except ValueError:
+				continue
+			avgIdx += 1
 
-		yaw = yawAvg / AVERAGE_LENGTH
-		pitch = (pitchAvg / AVERAGE_LENGTH)*(-1)
-		roll = rollAvg / AVERAGE_LENGTH
-		zoom = zoomAvg / AVERAGE_LENGTH
+		yaw = yawAvg.sum() / AVERAGE_LENGTH
+		pitch = (pitchAvg.sum() / AVERAGE_LENGTH)*(-1)
+		roll = rollAvg.sum() / AVERAGE_LENGTH
+		zoom = zoomAvg.sum() / AVERAGE_LENGTH
 		#print yaw, pitch
 		if abs(pitch) > 90:
 			pitch = (180 - abs(pitch))*(pitch/abs(pitch))
